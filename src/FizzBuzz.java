1670834f28a0ef612591fe5d287b3e3df3521619
Jisuh Choi <jisuhchoi@jisuhs-mbp.home>
2024-10-28 14:49:47 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,13 +6,14 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 1; // Initialize the counter
-        while (i < 100) { // Loop until i reaches 100
-
-            i = dofizzbuzz(i);
+        while (i < 100) {
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int dofizzbuzz(int i) {
+    private static int doFizzBuzz(int i) {
+        // Loop until i reaches 100
+
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
